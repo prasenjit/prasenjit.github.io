606de4563b0fff4705017bb361c26cf09f8e120f
Adi Shavit <adi.shavit@autodesk.com>
2016-11-29 09:42:32 +0200

    Removes extranious scroll bar on main page.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ layout: null
 ---
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
+    $('.content-wrapper__inner').css('display', 'block')
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
@@ -16,10 +17,12 @@ $(document).ready(function () {
 
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
+    $('.content-wrapper__inner').css('display', 'block')
   }
 
   if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
+    $('.content-wrapper__inner').css('display', 'block')
   }
 
   $('.btn-mobile-menu').click(function () {
